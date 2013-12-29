006dd5a5fc6b8a7452f97fd89fa102de9c5b0ed6
Steven Noe <git@killerdesigner.com>
2013-12-28 16:24:09 -0800

    Delete secret_token.rb

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-HartlApp::Application.config.secret_token = '7f1678aa58a1a85c23d57b7311059f0f70272353d4d53c5fce714a4513f5923a20a8299a0f4763eb8d8737ff9c05edd0e51206422eb056ebb8937b0571f735e1'
